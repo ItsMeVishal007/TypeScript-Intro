3ed22a367d46161d436958ee32e98e21ef928b0a
Cd-Vishal007 <vishal.codedrill@gmail.com>
2021-08-06 19:51:47 +0530

    more functional stuff

--- annotation/variables.ts
+++ annotation/variables.ts
@@ -7,3 +7,19 @@ let date: Date = new Date();
 let color: string[] = ["red", "blue", "green"];
 let numArray: number[] = [1, 2, 3];
 let booleanArr: boolean[] = [true, false, true];
+
+class fruit {}
+let orange: fruit = new fruit();
+
+// objects literals
+
+let points: { x: number; y: number } = {
+  x: 20,
+  y: 10,
+};
+
+// Functions
+
+const addNum: (a: number, b: number) => number = (a: number, b: number) => {
+  return a + b;
+};
